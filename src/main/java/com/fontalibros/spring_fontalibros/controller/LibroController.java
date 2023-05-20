21ac55d8500d2018960a69d76a03f5320380f47e
MAXTERS <casemgithub@gmail.com>
2023-05-20 11:18:47 -0500

    Se creó la funcionalidad para editar los datos del libro

--- src/main/java/com/fontalibros/spring_fontalibros/controller/LibroController.java
+++ src/main/java/com/fontalibros/spring_fontalibros/controller/LibroController.java
@@ -1,10 +1,13 @@
 package com.fontalibros.spring_fontalibros.controller;
 
+import java.util.Optional;
+
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -33,6 +36,7 @@ public class LibroController {
 	
 	// Recibe la solicitud Get a la ruta base /libros y muestra la vista libros/show
 	// Pasamos como parametro un objeto de tipo model que nos va a llevar informacion del backend a la vista
+	
 	@GetMapping("")
 	public String show(Model model) {
 		model.addAttribute("libros", libroService.findAll());
@@ -45,13 +49,36 @@ public class LibroController {
 	}
 	
 	// Hacemos una peticion directamente al controlador libros para que nos cargue la vista show
+	
 	@PostMapping("/save")
 	public String save(Libro libro) {
 		LOGGER.info("este es el objeto libro de la vista {}",libro);
 		Usuario u = new Usuario(1, "", "", "", "", "", "", "", "");
 		libro.setUsuario(u);
-		
 		libroService.save(libro);
+		return "redirect:/libros";
+	}
+	
+	// Metodo para editar que busque el registro con el id que le pasemos
+	// La anotacion @PathVariable mapea la variable que le pasamos, en este caso el id y la pasamos al Integer id
+	
+	@GetMapping("/editar/{id}")
+	public String editar(@PathVariable Integer id, Model model) {
+		Libro libro = new Libro();
+		Optional<Libro> optionalLibro = libroService.get(id);
+		libro = optionalLibro.get();
+		
+		LOGGER.info("Libro buscado: {}", libro);
+		model.addAttribute("libro", libro);
+		
+		return "libros/editar";
+	}
+	
+	// Metodo para actualizar los datos del libro
+	
+	@PostMapping("/actualizar")
+	public String actualizar(Libro libro) {
+		libroService.update(libro);
 		return "redirect:/libros";
 	}
 }
