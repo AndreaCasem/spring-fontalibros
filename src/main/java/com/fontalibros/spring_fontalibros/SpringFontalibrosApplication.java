ee526f2c726722626412139a5e061eb7d7aaacb8
AndreaCasem <casemgithub@gmail.com>
2023-08-13 15:32:16 -0500

    Generando el archivo jar y el archivo Dockerfile del proyecto

--- src/main/java/com/fontalibros/spring_fontalibros/SpringFontalibrosApplication.java
+++ src/main/java/com/fontalibros/spring_fontalibros/SpringFontalibrosApplication.java
@@ -7,6 +7,8 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 public class SpringFontalibrosApplication {
+	
+	public String PORT= System.getenv("PORT");
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringFontalibrosApplication.class, args);
